ec6890a2c863ab25a22bd5c5cba95a7a52feb60b
Tim Carr <tim@n7studios.co.uk>
2022-11-24 15:43:59 +0000

    Tests for forms using new provider/integration

--- tests/_support/Helper/Acceptance/WPForms.php
+++ tests/_support/Helper/Acceptance/WPForms.php
@@ -10,6 +10,30 @@ namespace Helper\Acceptance;
 class WPForms extends \Codeception\Module
 {
 	/**
+	 * Helper method to setup a ConvertKit integration in WPForms with a valid API Key and Secret
+	 *
+	 * @since   1.4.0
+	 *
+	 * @param   AcceptanceTester $I  AcceptanceTester.
+	 */
+	public function setupWPFormsIntegration($I)
+	{
+		$I->haveOptionInDatabase(
+			'wpforms_providers',
+			[
+				'convertkit' => [
+					'63725bdcceea3' => [
+						'api_key'    => $_ENV['CONVERTKIT_API_KEY'],
+						'api_secret' => $_ENV['CONVERTKIT_API_SECRET'],
+						'label'      => 'ConvertKit',
+						'date'       => strtotime('now'),
+					],
+				],
+			]
+		);
+	}
+
+	/**
 	 * Creates a WPForms Form.
 	 *
 	 * @since   1.4.0
@@ -48,7 +72,13 @@ class WPForms extends \Codeception\Module
 		$I->click('#wpforms-template-simple-contact-form-template a.wpforms-template-select');
 
 		// Wait for form editor to load.
-		$I->waitForElementVisible('#wpforms-builder-form');
+		$I->waitForElementVisible('button#wpforms-add-fields-text');
+
+		// Add Tag Field.
+		$I->click('button#wpforms-add-fields-text');
+		$I->waitForElementVisible('.wpforms-field-text');
+		$I->click('.wpforms-field-text');
+		$I->fillField('.wpforms-field-option-text .active input[type=text]', 'Tag ID');
 
 		// Click Save.
 		$I->waitForElementVisible('#wpforms-save');
@@ -68,85 +98,60 @@ class WPForms extends \Codeception\Module
 	 *
 	 * @param   AcceptanceTester $I             AcceptanceTester.
 	 * @param   int              $wpFormID      WPForms Form ID.
-	 * @param   bool|string      $apiKey        API Key.
-	 * @param   bool|int         $formID        ConvertKit Form ID.
+	 * @param   bool|string      $formName      ConvertKit Form Name.
 	 * @param   bool|string      $nameField     First Name Field.
 	 * @param   bool|string      $emailField    Email Address Field.
-	 * @param   bool|string      $customField   Custom Field (if specified, adds a field whose value will be used as a ConvertKit Custom Field Value).
-	 * @param   bool|string      $tagField      Tag Field (if specified, adds a field whose value will be used as a ConvertKit Tag).
+	 * @param   bool|array       $customFields  Custom Fields.
+	 * @param   bool|string      $tagField      Tag Field.
 	 */
-	public function configureConvertKitSettingsOnForm($I, $wpFormID, $apiKey = false, $formID = false, $nameField = false, $emailField = false, $customField = false, $tagField = false)
+	public function configureConvertKitSettingsOnForm($I, $wpFormID, $formName, $nameField = false, $emailField = false, $customFields = false, $tagField = false)
 	{
 		// Load WPForms Editor.
 		$I->amOnAdminPage('admin.php?page=wpforms-builder&view=fields&form_id=' . $wpFormID);
 
-		// Click Settings icon.
-		$I->waitForElementVisible('.wpforms-panel-settings-button');
-		$I->click('.wpforms-panel-settings-button');
+		// Click Marketing icon.
+		$I->waitForElementVisible('.wpforms-panel-providers-button');
+		$I->click('.wpforms-panel-providers-button');
 
 		// Click ConvertKit tab.
-		$I->click('.wpforms-panel-sidebar a.wpforms-panel-sidebar-section-be_convertkit');
+		$I->click('#wpforms-panel-providers a.wpforms-panel-sidebar-section-convertkit');
+
+		// Click Add New Connection.
+		$I->click('Add New Connection');
+
+		// Define name for connection.
+		$I->waitForElementVisible('.jconfirm-content');
+		$I->fillField('#provider-connection-name', 'ConvertKit');
+		$I->click('OK');
+
+		// Get the connection ID.
+		$I->waitForElementVisible('.wpforms-provider-connections .wpforms-provider-connection');
+		$connectionID = $I->grabAttributeFrom('.wpforms-provider-connections .wpforms-provider-connection', 'data-connection_id');
 
 		// Specify field values.
-		if ($apiKey) {
-			$I->fillField('#wpforms-panel-field-settings-be_convertkit_api', $apiKey);
-		}
-		if ($formID) {
-			$I->fillField('#wpforms-panel-field-settings-be_convertkit_form_id', $formID);
-		}
-		if ($nameField) {
-			$I->selectOption('#wpforms-panel-field-settings-be_convertkit_field_first_name', $nameField);
+		$I->waitForElementVisible('div[data-connection_id="' . $connectionID . '"] .wpforms-provider-fields');
+
+		if ($formName) {
+			$I->selectOption('providers[convertkit][' . $connectionID . '][list_id]', $formName);
+
+			// Wait for field mappings to reload, as the ConvertKit Form has changed.
+			$I->waitForElementVisible('div[data-connection_id="' . $connectionID . '"] .wpforms-provider-fields');
 		}
 		if ($emailField) {
-			$I->selectOption('#wpforms-panel-field-settings-be_convertkit_field_email', $emailField);
+			$I->selectOption('providers[convertkit][' . $connectionID . '][fields][email]', $emailField);
 		}
-
-		// Custom Field.
-		if ($customField) {
-			// Click Fields icon.
-			$I->click('.wpforms-panel-fields-button');
-
-			// Add Single Linke Text field.
-			$I->click('#wpforms-add-fields-text');
-
-			// Click field.
-			$I->waitForElementVisible('.wpforms-field-text');
-			$I->click('.wpforms-field-text');
-
-			// Enter field name.
-			$I->waitForElementVisible('.wpforms-field-option-text .active');
-			$I->fillField('.wpforms-field-option-text .active input[type=text]', 'Custom Field Value');
-
-			// Click Advanced tab.
-			$I->click('.wpforms-field-option-text .wpforms-field-option-group-advanced a.wpforms-field-option-group-toggle');
-
-			// Add CSS class to tell Plugin that the value of this field is a custom field.
-			$I->waitForElementVisible('.wpforms-field-option-text .active');
-			$I->fillField('.wpforms-field-option-text .wpforms-field-option-row-css input[type=text]', 'ck-custom-' . $customField);
+		if ($nameField) {
+			$I->selectOption('providers[convertkit][' . $connectionID . '][fields][name]', $nameField);
 		}
-
-		// Tag Field.
 		if ($tagField) {
-			// Click Fields icon.
-			$I->click('.wpforms-panel-fields-button');
+			$I->selectOption('providers[convertkit][' . $connectionID . '][fields][tag]', $tagField);
+		}
 
-			// Add Single Linke Text field.
-			$I->click('#wpforms-add-fields-text');
-
-			// Click field.
-			$I->waitForElementVisible('.wpforms-field-text');
-			$I->click('.wpforms-field-text');
-
-			// Enter field name.
-			$I->waitForElementVisible('.wpforms-field-option-text .active');
-			$I->fillField('.wpforms-field-option-text .active input[type=text]', 'Tag ID');
-
-			// Click Advanced tab.
-			$I->click('.wpforms-field-option-text .wpforms-field-option-group-advanced a.wpforms-field-option-group-toggle');
-
-			// Add CSS class to tell Plugin that the value of this field is a tag field.
-			$I->waitForElementVisible('.wpforms-field-option-text .active');
-			$I->fillField('.wpforms-field-option-text .wpforms-field-option-row-css input[type=text]', 'ck-tag');
+		// Custom Fields.
+		if ($customFields) {
+			foreach ($customFields as $customField => $customFieldValue) {
+				$I->selectOption('providers[convertkit][' . $connectionID . '][fields][custom_field_' . $customField . ']', $customFieldValue);
+			}
 		}
 
 		// Click Save.
