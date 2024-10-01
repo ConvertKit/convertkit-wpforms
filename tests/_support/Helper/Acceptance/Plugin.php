fd7a7ea4b6045207ce71dcd4ea44c13e1759a8f3
Tim Carr <tim@n7studios.co.uk>
2024-10-01 10:48:06 +0800

    Tests: Change `activateConvertKitPlugin` and `deactivateConvertKitPlugin` helpers to reflect title change

--- tests/_support/Helper/Acceptance/Plugin.php
+++ tests/_support/Helper/Acceptance/Plugin.php
@@ -19,7 +19,7 @@ class Plugin extends \Codeception\Module
 	 */
 	public function activateConvertKitPlugin($I)
 	{
-		$I->activateThirdPartyPlugin($I, 'integrate-convertkit-wpforms');
+		$I->activateThirdPartyPlugin($I, 'integrate-kit-formerly-convertkit-wpforms');
 	}
 
 	/**
@@ -32,7 +32,7 @@ class Plugin extends \Codeception\Module
 	 */
 	public function deactivateConvertKitPlugin($I)
 	{
-		$I->deactivateThirdPartyPlugin($I, 'integrate-convertkit-wpforms');
+		$I->deactivateThirdPartyPlugin($I, 'integrate-kit-formerly-convertkit-wpforms');
 	}
 
 	/**
